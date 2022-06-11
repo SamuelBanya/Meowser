6a1bf7cb18cea713c81e913895e091d1406798c4
Samuel Banya <sbanya@fastmail.com>
2022-06-11 18:56:34 -0400

    Adding revisions to include 'json-server' dependency

--- src/js/meowser.js
+++ src/js/meowser.js
@@ -1,105 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let zipcodeSearchButton = document.querySelector("#zipcodeSearchButton");
-    console.log("zipcodeSearchButton: ", zipcodeSearchButton);
-    zipcodeSearchButton.addEventListener("click", (e) => {
-        let zipcodeSearchInputValue = document.querySelector("#zipcodeSearchInput").value;
-        console.log(`zipcodeSearchInputValue: ${zipcodeSearchInputValue}`);
+    let phraseFormSubmit = document.querySelector("#phraseFormSubmit");
+    phraseFormSubmit.addEventListener("click", (e) => {
+        let phraseInputValue = document.querySelector("#phraseInput").value;
+        console.log(`phraseInputValue: ${phraseInputValue}`);
     })
-    testRequest();
+    displayWikiCatBreeds();
 });
 
-function testRequest() {
-    let accessResponse;
-    fetch("https://api.petfinder.com/v2/oauth2/token", {
-        method: "POST",
-        headers: {
-            "content-type": "application/json"
-        },
-        body: JSON.stringify({
-            grant_type: "client_credentials",
-            client_id: `${APIKEY}`,
-            client_secret: `${SECRET}`,
-        })
-    })
+function displayWikiCatBreeds() {
+    fetch("http://localhost:3000/breeds")
         .then(response => response.json())
-        .then(response => {
-            accessResponse = response;
-            console.log("accessResponse: ", accessResponse);
-            return accessResponse;
+        .then(data => {
+            console.log("data: ");
+            console.log(data);
         })
-    // fetch("https://api.petfinder.com/v2/{CATEGORY}/{ACTION}?{parameter_1}={value_1}&{parameter_2}={value_2}", {
-        .then(() => {
-            console.log("accessResponse:");
-            console.log(accessResponse);
-            console.log("accessToken: ");
-            let accessToken = accessResponse["access_token"];
-            console.log(accessToken);
-            fetch("https://api.petfinder.com/v2/animals", {
-                headers: {
-                    Authorization: `Bearer: ${accessToken}`
-                }
-            })
-                .then(response => response.json())
-                .then(response => {
-                    console.log("response: ", response);
-                })
-
-        });
 }
-document.addEventListener("DOMContentLoaded", () => {
-    let zipcodeSearchButton = document.querySelector("#zipcodeSearchButton");
-    console.log("zipcodeSearchButton: ", zipcodeSearchButton);
-    zipcodeSearchButton.addEventListener("click", (e) => {
-        let zipcodeSearchInputValue = document.querySelector("#zipcodeSearchInput").value;
-        console.log(`zipcodeSearchInputValue: ${zipcodeSearchInputValue}`);
-    })
-    testRequest();
-});
-
-function testRequest() {
-    let accessResponse;
-    fetch("https://api.petfinder.com/v2/oauth2/token", {
-        method: "POST",
-        headers: {
-            "content-type": "application/json"
-        },
-        body: JSON.stringify({
-            grant_type: "client_credentials",
-            client_id: `${APIKEY}`,
-            client_secret: `${SECRET}`,
-        })
-    })
-        .then(response => response.json())
-        .then(response => {
-            accessResponse = response;
-            console.log("accessResponse: ", accessResponse);
-            return accessResponse;
-        })
-    // fetch("https://api.petfinder.com/v2/{CATEGORY}/{ACTION}?{parameter_1}={value_1}&{parameter_2}={value_2}", {
-        .then(() => {
-            console.log("accessResponse:");
-            console.log(accessResponse);
-            console.log("accessToken: ");
-            let accessToken = accessResponse["access_token"];
-            console.log(accessToken);
-            fetch("https://api.petfinder.com/v2/animals", {
-                headers: {
-                    Authorization: `Bearer: ${accessToken}`
-                }
-            })
-                .then(response => response.json())
-                .then(response => {
-                    console.log("response: ", response);
-                })
-p
-        });
-}
-
-
-function obtainCatBreedsFromWikipedia() {
-    let avoid list = []
-    fetch("")
-    // These are the specific results to avoid:
-    if (! breed[0] === "[") {
-
-    }
