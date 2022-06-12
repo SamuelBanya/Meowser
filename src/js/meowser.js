c22a8a3b8b5b18d00b8f46b3d7c56dd5e1af2855
Samuel Banya <sbanya@fastmail.com>
2022-06-12 14:35:04 -0400

    Slowly adding ability to grab images from Wiki, and to default by image in 'db.json' later on

--- src/js/meowser.js
+++ src/js/meowser.js
@@ -1,27 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
     // Grab cat breeds from 'db.json':
     displayWikiCatBreeds();
+
+
+    let catImagesButton = document.querySelector("#catImagesButton");
+
+    catImagesButton.addEventListener("click", (e) => {
+        e.preventDefault();
+
+        console.log("'catImagesButton' button clicked!");
+
+        let breedSelectTag = document.querySelector("#breed-select");
+        let breedLink = breedSelectTag.options[breedSelectTag.selectedIndex].value;
+        breedLink = breedLink.toString().replace(/\s/g, '');
+        console.log(`breedLink: ${breedLink}`);
+        console.log(breedLink);
+        // Place stored image link into 'resultImage' image in 'rightResultsMenu' section:
+        let resultImage = document.querySelector("#resultImage");
+
+        // resultImage.src =
+    })
+
     let catWikiButton = document.querySelector("#catWikiButton");
+
     catWikiButton.addEventListener("click", (e) => {
         e.preventDefault();
 
         console.log("'catWikiButton' button clicked!");
 
         let breedSelectTag = document.querySelector("#breed-select");
-        // Used this reference to be able to drill down into 'select' tag's value:
-        // https://www.javascripttutorial.net/javascript-dom/javascript-select-box/
-        let breedValue = breedSelectTag.options[breedSelectTag.selectedIndex].value;
-        // Used this example as a basis on how to use .replace() to rip out
-        // spaces:
-        // https://stackoverflow.com/questions/5963182/how-to-remove-spaces-from-a-string-using-javascript
-        breedValue = breedValue.toString().replace(/\s/g, '');
-        console.log(`breedValue: ${breedValue}`);
-        console.log(breedValue);
-        // Make API call to https://cataas.com/ website with this format:
-        // https://cataas.com/cat/<tag>
-        // More specifically to include 'phraseInputValue':
-        // https://cataas.com/cat/tabby/says/phraseInputValue
-        // fetch(`https://cataas.com/cat/${breedValue}/${phraseInputValue}`)
+        let breedLink = breedSelectTag.options[breedSelectTag.selectedIndex].value;
+        breedLink = breedLink.toString().replace(/\s/g, '');
+        console.log(`breedLink: ${breedLink}`);
+        console.log(breedLink);
+        // Make API call to 'MediaWiki' REST API using 'breedLink'
         fetch(`https://cataas.com/cat/${breedValue}`)
             .then(response => {
                 console.log(response);
@@ -44,7 +56,7 @@ function displayWikiCatBreeds() {
             data.forEach((catBreed) => {
                 console.log(`name: ${catBreed["name"]}`);
                 let optionTag = document.createElement("option");
-                optionTag.value = catBreed["name"];
+                optionTag.value = catBreed["wikiArticleLink"];
                 optionTag.textContent = catBreed["name"];
                 breedSelectTag.append(optionTag);
             })
